c3a5acaef789474fc0ae84c0948b498f6337d59d
letsplayit24x7 <113431854+letsplayit24x7@users.noreply.github.com>
2022-09-22 18:42:12 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ import { Component,OnInit } from '@angular/core';
 export class AppComponent {
   title = 'angular-quickstart';
    ngOnInit(): void {
+    let document = window.document;
     document.querySelector('circle').addEventListener('click', (e: any) => {
       const randomColour = Math.round(Math.random() * 0xffffff);
-      e.target.style.stroke = `#${randomColour.toString(16).padStart(6, '0')}`;
+      e.target.style.stroke = `#${randomColour.toString(16)}`;
     });
   }
 }
